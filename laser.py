bf3ec4b87b4f59aaaa56678f93da7e31bbabe394
Vinay Sharma <vinay04sharma@icloud.com>
2017-05-08 15:51:04 +0530

    improved laser.py

--- laser.py
+++ laser.py
@@ -5,31 +5,36 @@ import RPi.GPIO as GPIO
 import socket 
 import sys
 from thread import *
-import json
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(19, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
 
-GPIO.output(5, GPIO.HIGH)
-GPIO.output(6, GPIO.HIGH)
-GPIO.output(26, GPIO.HIGH)
+red_lower = np.array([0, 0, 240])
+red_upper = np.array([10, 10, 255])
 
-PWMR = GPIO.PWM(17, 60)
-PWMR1 = GPIO.PWM(18, 60)
-PWML = GPIO.PWM(19, 60)
-PWML1 = GPIO.PWM(13, 60)
 
-PWML.start(0)
-PWML1.start(0)
-PWMR.start(0)
-PWMR1.start(0)
+def init():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(17, GPIO.OUT)
+	GPIO.setup(18, GPIO.OUT)
+	GPIO.setup(19, GPIO.OUT)
+	GPIO.setup(13, GPIO.OUT)
+	GPIO.setup(5, GPIO.OUT)
+	GPIO.setup(6, GPIO.OUT)
+	GPIO.setup(26, GPIO.OUT)
+	
+	GPIO.output(5, GPIO.HIGH)
+	GPIO.output(6, GPIO.HIGH)
+	GPIO.output(26, GPIO.HIGH)
+	
+	PWMR = GPIO.PWM(17, 60)
+	PWMR1 = GPIO.PWM(18, 60)
+	PWML = GPIO.PWM(19, 60)
+	PWML1 = GPIO.PWM(13, 60)
+
+	PWML.start(0)
+	PWML1.start(0)
+	PWMR.start(0)
+	PWMR1.start(0)
 
 def makesocket():
         host = ''
@@ -80,37 +85,16 @@ def communicate(sock, reply):
 	time.sleep(2)
 
 
-cap = cv2.VideoCapture(0)
-red_lower = np.array([0, 0, 240])
-red_upper = np.array([10, 10, 255])
-
-sock = makesocket()
-
-mydict = {"laser": 1, "color": 6}
-
-while cv2.waitKey(1) != 27:
-	ret, img = cap.read()
-        h, w = img.shape;
-	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	communicate(sock, json.dumps(mydict))
-
-	mask = cv2.inRange(hsv, red_lower, red_upper)
-	contours,hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	cv2.imshow("mask", mask)
+def driveMotors(cx, cy, h, w):
 	
-	cx = w/2
-	cy = h/2
-	if contours:	
-		cnts = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
-		M = cv2.moments(np.array(cnts[0]))
-		if M["m00"] != 0:
-			cx = int(M['m10']/M['m00'])
-			cy = int(M['m01']/M['m00'])
-			cv2.circle(img, (cx, cy), 9, (0, 255, 0), -1)
-        cv2.imshow("img", img)
-	motor = ( ( (240-cy)/220.0 )*100, ( (240-cy)/220.0 )*100 )
-	cx = ((cx-320)/300.0)*100
+	offset = 20.0
+	
+	motor = ( ( (h/2 - cy)/(h/2 - offset) )*100, ((h/2-cy)/(h/2-offset))*100)
+	
+	cx = ((cx-w/2)/(w/2 - offset))*100
+	
 	motor = (motor[0] + cx, motor[1] - cx)
+	
 	if (motor[0] < 0):
 		if (motor[0] < 100):
 			PWML.ChangeDutyCycle(100)
@@ -123,6 +107,7 @@ while cv2.waitKey(1) != 27:
 		else:
 			PWML1.ChangeDutyCycle(motor[0])
 		PWML.ChangeDutyCycle(0)
+	
 	if (motor[1] < 0):
 		if (motor[1] < 100):
 			PWMR.ChangeDutyCycle(100)	
@@ -139,6 +124,44 @@ while cv2.waitKey(1) != 27:
 
 
 
+def follow():
 
+	cap = cv2.VideoCapture(0)
+	
+	while cv2.waitKey(1) != 27:
+		ret, img = cap.read()
+	    h, w = img.shape
+		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		
+	
+		mask = cv2.inRange(hsv, red_lower, red_upper)
+		contours,hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		
+		
+		cx = w/2
+		cy = h/2
+		if not contours:	
+			communicate(sock, "Laser not visible")
+			
+		else:
+			cnts = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+			M = cv2.moments(np.array(cnts[0]))
+			if M["m00"] != 0:
+				cx = int(M['m10']/M['m00'])
+				cy = int(M['m01']/M['m00'])
+				cv2.circle(img, (cx, cy), 9, (0, 255, 0), -1)
+				communicate(sock, "Following")
+				driveMotors(cx, cy, h, w)
+			else:
+				communicate(sock, "Laser not visible")
+				
+	    #cv2.imshow("img", img)
+				
+
+
+sock = makesocket()
+init()
+follow()
+	
 
 
